513b957fdf50f9fd3141a9d5bc582efc4fcc447b
yongxu <yongxu@cecelive.com>
2018-10-14 23:32:14 +0800

    first laravel

---
+++ app/Http/Controllers/Test/TestController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liubo
+ * Date: 2018/5/5
+ * Time: 下午11:47
+ */
+
+namespace App\Http\Controllers\Test;
+
+
+use App\Http\Controllers\Controller;
+
+class TestController extends Controller
+{
+    public function test()
+    {
+        echo 1;
+    }
+}
